b54f9665884d932353d76a564b7d6d423a07010c
Matt <matt@artemisbot.uk>
2019-03-05 16:24:59 +0000

    not actually fixed

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -15,7 +15,7 @@ doxygen $DOXYFILE 2>&1 | tee doxygen.log
 if [ -d "html" ] && [ -f "html/index.html" ]; then
 	git add --all
 	git commit -m "Deploy docs: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
-	git push--force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
+	git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
 else
 	echo '' >&2
 	echo 'Warning: No documentation (html) files have been found!' >&2
